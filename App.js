d1306b23b1a00e41f8d0ceae43baf143006c09e7
Pedro Castro <pcastro9876@gmail.com>
2024-10-30 11:44:03 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
+  const [input, setInput] = useState('');
+  const [nome, setNome] = useState('');
+
+  useEffect(() => {
+    async function loadData () {
+      await  AsyncStorage.getItem('@nome').then((value)=> {
+        setNome(value);
+      })
+    }
+
+    loadData();
+  },[])
+
+  async function gravaNome () {
+    await AsyncStorage.setItem('@nome', input)
+    setNome(input);
+
+    setInput('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.viewInput}>
+          <TextInput
+          style={styles.input}
+          value={input}
+          onChangeText={(texto) => setInput(texto)}
+          />
+
+          <TouchableOpacity onPress={gravaNome}>
+            <Text style={styles.btn}>+</Text>
+          </TouchableOpacity>
+      </View>
+      <Text style={styles.nome}>{nome}</Text>
     </View>
   );
 }
@@ -15,6 +47,30 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 45
   },
+  viewInput: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  input: {
+    width: 350,
+    height: 40,
+    borderColor: '#000',
+    borderWidth: 1,
+    padding: 10,
+
+  },
+  btn: {
+    backgroundColor: '#222',
+    color: '#fff',
+    height: 40,
+    padding: 10,
+    marginLeft: 4
+  },
+  nome: {
+    fontSize: 30,
+    color: '#000',
+    marginTop: 30
+  }
 });
